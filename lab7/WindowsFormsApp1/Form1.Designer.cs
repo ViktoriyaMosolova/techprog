18c5aa47c5090644cbe25ed3f6fd09556b91d6e7
unknown <mosolova.viktoriya@mail.ru>
2023-04-11 16:46:54 +0300

    lab7

--- lab7/WindowsFormsApp1/Form1.Designer.cs
+++ lab7/WindowsFormsApp1/Form1.Designer.cs
@@ -43,10 +43,12 @@
             this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
             this.numericUpDown6 = new System.Windows.Forms.NumericUpDown();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.groubBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
@@ -55,16 +57,32 @@
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // numericUpDown1
             // 
             this.numericUpDown1.DecimalPlaces = 10;
-            this.numericUpDown1.Location = new System.Drawing.Point(18, 19);
+            this.numericUpDown1.Location = new System.Drawing.Point(6, 19);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown1.TabIndex = 0;
             this.numericUpDown1.ThousandsSeparator = true;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // groubBox1
             // 
@@ -76,7 +94,7 @@
             this.groubBox1.Controls.Add(this.numericUpDown1);
             this.groubBox1.Location = new System.Drawing.Point(12, 12);
             this.groubBox1.Name = "groubBox1";
-            this.groubBox1.Size = new System.Drawing.Size(200, 100);
+            this.groubBox1.Size = new System.Drawing.Size(162, 100);
             this.groubBox1.TabIndex = 1;
             this.groubBox1.TabStop = false;
             this.groubBox1.Text = "X";
@@ -84,7 +102,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(144, 73);
+            this.label3.Location = new System.Drawing.Point(132, 73);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(20, 13);
             this.label3.TabIndex = 6;
@@ -93,7 +111,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(144, 47);
+            this.label2.Location = new System.Drawing.Point(132, 47);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(20, 13);
             this.label2.TabIndex = 5;
@@ -102,7 +120,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(144, 21);
+            this.label1.Location = new System.Drawing.Point(132, 21);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(20, 13);
             this.label1.TabIndex = 4;
@@ -111,7 +129,12 @@
             // numericUpDown3
             // 
             this.numericUpDown3.DecimalPlaces = 10;
-            this.numericUpDown3.Location = new System.Drawing.Point(18, 71);
+            this.numericUpDown3.Location = new System.Drawing.Point(6, 71);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
             this.numericUpDown3.Name = "numericUpDown3";
             this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown3.TabIndex = 2;
@@ -125,7 +148,17 @@
             // numericUpDown2
             // 
             this.numericUpDown2.DecimalPlaces = 10;
-            this.numericUpDown2.Location = new System.Drawing.Point(18, 45);
+            this.numericUpDown2.Location = new System.Drawing.Point(6, 45);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
             this.numericUpDown2.Name = "numericUpDown2";
             this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown2.TabIndex = 1;
@@ -144,9 +177,9 @@
             this.groupBox2.Controls.Add(this.numericUpDown4);
             this.groupBox2.Controls.Add(this.numericUpDown5);
             this.groupBox2.Controls.Add(this.numericUpDown6);
-            this.groupBox2.Location = new System.Drawing.Point(12, 124);
+            this.groupBox2.Location = new System.Drawing.Point(12, 118);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.Size = new System.Drawing.Size(162, 100);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Y";
@@ -154,7 +187,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(144, 73);
+            this.label6.Location = new System.Drawing.Point(132, 73);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(20, 13);
             this.label6.TabIndex = 6;
@@ -163,7 +196,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(144, 47);
+            this.label5.Location = new System.Drawing.Point(132, 45);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(20, 13);
             this.label5.TabIndex = 5;
@@ -172,7 +205,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(144, 21);
+            this.label4.Location = new System.Drawing.Point(132, 21);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(20, 13);
             this.label4.TabIndex = 5;
@@ -181,7 +214,12 @@
             // numericUpDown4
             // 
             this.numericUpDown4.DecimalPlaces = 10;
-            this.numericUpDown4.Location = new System.Drawing.Point(18, 71);
+            this.numericUpDown4.Location = new System.Drawing.Point(6, 71);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
             this.numericUpDown4.Name = "numericUpDown4";
             this.numericUpDown4.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown4.TabIndex = 2;
@@ -195,7 +233,17 @@
             // numericUpDown5
             // 
             this.numericUpDown5.DecimalPlaces = 10;
-            this.numericUpDown5.Location = new System.Drawing.Point(18, 45);
+            this.numericUpDown5.Location = new System.Drawing.Point(6, 45);
+            this.numericUpDown5.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
             this.numericUpDown5.Name = "numericUpDown5";
             this.numericUpDown5.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown5.TabIndex = 1;
@@ -209,11 +257,26 @@
             // numericUpDown6
             // 
             this.numericUpDown6.DecimalPlaces = 10;
-            this.numericUpDown6.Location = new System.Drawing.Point(18, 19);
+            this.numericUpDown6.Location = new System.Drawing.Point(6, 19);
+            this.numericUpDown6.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown6.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
             this.numericUpDown6.Name = "numericUpDown6";
             this.numericUpDown6.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown6.TabIndex = 0;
             this.numericUpDown6.ThousandsSeparator = true;
+            this.numericUpDown6.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // checkedListBox1
             // 
@@ -225,24 +288,17 @@
             "Tan",
             "Ln",
             "Lg"});
-            this.checkedListBox1.Location = new System.Drawing.Point(218, 20);
+            this.checkedListBox1.Location = new System.Drawing.Point(180, 20);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(134, 169);
+            this.checkedListBox1.Size = new System.Drawing.Size(83, 109);
             this.checkedListBox1.TabIndex = 4;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(358, 20);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(430, 338);
-            this.textBox1.TabIndex = 5;
+            this.checkedListBox1.SelectedValueChanged += new System.EventHandler(this.checkedListBox1_SelectedValueChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(399, 364);
+            this.button1.Location = new System.Drawing.Point(288, 364);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 35);
+            this.button1.Size = new System.Drawing.Size(142, 35);
             this.button1.TabIndex = 6;
             this.button1.Text = "Рассчитать";
             this.button1.UseVisualStyleBackColor = true;
@@ -250,31 +306,64 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(523, 364);
+            this.button2.Location = new System.Drawing.Point(438, 364);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(118, 35);
+            this.button2.Size = new System.Drawing.Size(142, 35);
             this.button2.TabIndex = 7;
-            this.button2.Text = "В файл...";
+            this.button2.Text = "Перезаписать в файл";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(647, 364);
+            this.button3.Location = new System.Drawing.Point(586, 364);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(118, 35);
+            this.button3.Size = new System.Drawing.Size(144, 35);
             this.button3.TabIndex = 8;
             this.button3.Text = "Закрыть";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 224);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(251, 204);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(438, 405);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(142, 35);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Дописать в файл";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(269, 20);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox1.Size = new System.Drawing.Size(504, 338);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.WordWrap = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groubBox1);
@@ -290,6 +379,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -312,10 +402,12 @@
         private System.Windows.Forms.NumericUpDown numericUpDown5;
         private System.Windows.Forms.NumericUpDown numericUpDown6;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- lab7/WindowsFormsApp1/Form1.cs
+++ lab7/WindowsFormsApp1/Form1.cs
@@ -1,15 +1,17 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WindowsFormsApp1
 {
@@ -20,6 +22,48 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
+        List<string> Res = new List<string>();
+        public void Calc()
+        {
+            double X0 = ((double)numericUpDown1.Value);
+            double Xn = ((double)numericUpDown2.Value);
+            double Hx = ((double)numericUpDown3.Value);
+            double x = X0;
+            double Nx = (Xn - X0) / Hx;
+
+            double Y0 = ((double)numericUpDown6.Value);
+            double Yn = ((double)numericUpDown5.Value);
+            double Hy = ((double)numericUpDown4.Value);
+            double y = Y0;
+            double Ny = (Yn - Y0) / Hy;
+
+            string textresfile = "";
+            textresfile += string.Format("{0, -10}", "x\\y");
+            for (int i = 1; i <= Ny + 1; y = Y0 + i * Hy, i++)
+            {
+                textresfile += string.Format("{0, -10}", y);
+            }
+            textresfile += Environment.NewLine;
+            for (int i = 1; i <= Nx + 1; x = X0 + i * Hx, i++)
+            {
+                textresfile += string.Format("{0, -10}", x);
+                y = Y0;
+                for (int j = 1; j <= Ny + 1; y = Y0 + j * Hy, j++)
+                {
+                    if (Math.Round(y * Func(x), 2).ToString().Length > 5)
+                    {
+                        textresfile += string.Format("{0, -10}", (y * Func(x)).ToString("0.00E+0"));
+                    }
+                    else
+                    {
+                        textresfile += string.Format("{0, -10}", Math.Round(y * Func(x), 2).ToString());
+                    }
+                }
+                textresfile += Environment.NewLine;
+            }
+            Res.Add(textresfile);
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             if (checkedListBox1.CheckedItems.Count == 0)
@@ -28,34 +72,9 @@ namespace WindowsFormsApp1
             }
             else
             {
-                textBox1.Text = $"{"x\\y",-10}";
-
-                double X0 = ((double)numericUpDown1.Value);
-                double Xn = ((double)numericUpDown2.Value);
-                double Hx = ((double)numericUpDown3.Value);
-                double x = X0;
-                //Nx=
-
-                double Y0 = ((double)numericUpDown6.Value);
-                double Yn = ((double)numericUpDown5.Value);
-                double Hy = ((double)numericUpDown4.Value);
-                double y = Y0;
-
-                for (int j = 1; y <= Yn; y = Y0 + j * Hy, j++)
-                { 
-                    textBox1.AppendText($"{y,10}");
-                }
-                textBox1.AppendText(Environment.NewLine);
-                for (int i = 1; x <= Xn; x = X0 + i * Hx, i++)
-                {
-                    textBox1.AppendText($"{x,10:0.00}");
-                    y = Y0;
-                    for (int j = 1; y <= Yn; y = Y0 + j * Hy, j++)
-                    {
-                        textBox1.AppendText($"{y*Func(x),10:0.00}");
-                    }
-                    textBox1.AppendText(Environment.NewLine);
-                }
+                textBox1.Clear();
+                Calc();
+                textBox1.Text = (Res[Res.Count - 1]);
             }
         }
         public double Func(double x)
@@ -65,27 +84,94 @@ namespace WindowsFormsApp1
                 {
                     result += Math.Sin(x);
                 }
-                else if (checkedListBox1.GetItemChecked(1))
+                if (checkedListBox1.GetItemChecked(1))
                 {
                     result += Math.Cos(x) ;
                 }
-                else if (checkedListBox1.GetItemChecked(2))
+                if (checkedListBox1.GetItemChecked(2))
                 {
                     result += Math.Exp(x);
                 }
-                else if (checkedListBox1.GetItemChecked(3))
+                if (checkedListBox1.GetItemChecked(3))
                 {
                     result += Math.Tan(x);
                 }
-                else if (checkedListBox1.GetItemChecked(4))
+                if (checkedListBox1.GetItemChecked(4))
                 {
                     result += Math.Log(x);
                 }
-                else if (checkedListBox1.GetItemChecked(5))
+                if (checkedListBox1.GetItemChecked(5))
                 {
                     result += Math.Log10(x);
                 }
                 return result; 
+        }
+
+        private void checkedListBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            string resourcesFolder = Application.StartupPath + "\\Resources\\";
+            string[] imagePaths = { resourcesFolder + "sin.jpg",
+                                    resourcesFolder + "cos.jpg",
+                                    resourcesFolder + "exp.jpg",
+                                    resourcesFolder + "tan.jpg",
+                                    resourcesFolder + "ln.jpg",
+                                    resourcesFolder + "lg.jpg" };
+
+            if (checkedListBox1.SelectedIndex == 0)
+            {
+                pictureBox1.Image = Image.FromFile(imagePaths[0]);
+            }
+            else if (checkedListBox1.SelectedIndex == 1)
+            {
+                pictureBox1.Image = Image.FromFile(imagePaths[1]);
+            }
+            else if (checkedListBox1.SelectedIndex == 2)
+            {
+                pictureBox1.Image = Image.FromFile(imagePaths[2]);
+            }
+            else if (checkedListBox1.SelectedIndex == 3)
+            {
+                pictureBox1.Image = Image.FromFile(imagePaths[3]);
+            }
+            else if (checkedListBox1.SelectedIndex == 4)
+            {
+                pictureBox1.Image = Image.FromFile(imagePaths[4]);
+            }
+            else if (checkedListBox1.SelectedIndex == 5)
+            {
+                pictureBox1.Image = Image.FromFile(imagePaths[5]);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)//перезапись
+        {
+            textBox1.Clear();
+            Res.Clear();
+            Calc();
+            string fileoutput = Application.StartupPath + "\\OutputRes\\result.txt";
+            using (StreamWriter writer = new StreamWriter(fileoutput, false))
+            {
+                writer.WriteLine(Res[Res.Count - 1]);
+            }
+            MessageBox.Show("Значения в файле перезаписаны");
+        }
+
+        private void button4_Click(object sender, EventArgs e)//дописать
+        {
+            textBox1.Clear();
+            Calc();
+            string fileoutput = Application.StartupPath + "\\OutputRes\\result.txt";
+            using (StreamWriter writer = new StreamWriter(fileoutput, true))
+            {
+                writer.WriteLine(Res[Res.Count - 1]);
+            }
+            MessageBox.Show("Значения в файле дописаны");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
